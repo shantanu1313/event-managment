64d2eb9f6f1e661c6863947676ebe5ba3ca9852f
Gaurav Honde <gauravhonde66@gmail.com>
2026-01-22 18:30:45 +0530

    about
    about.ejs

--- routes/user_route.js
+++ routes/user_route.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/",function(req,res){
+router.get("/", function (req, res) {
     res.render('user/home.ejs');
+});
+
+router.get("/about", function (req, res) {
+    res.render('user/about.ejs');
 });
 
 module.exports = router;
