436b9fbcd63b57954fda08d2a1f0c77edc5525ee
Gaurav Honde <gauravhonde66@gmail.com>
2026-01-29 11:12:47 +0530

    dynamic blog page
    blog page dynamic

--- routes/admin_route.js
+++ routes/admin_route.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var exe = require("../connection");
 var router = express.Router();
 
 router.get("/", function (req, res) {
@@ -16,9 +17,170 @@ router.get("/package", function (req, res) {
 router.get("/gallery", function (req, res) {
     res.render('admin/gallery.ejs');
 });
-router.get("/blog", function (req, res) {
-    res.render('admin/blog.ejs');
+router.get("/blog", async function (req, res) {
+    var sql = "SELECT * FROM blog"
+    var data = await exe(sql)
+    var packet = { data };
+    res.render('admin/blog/blog.ejs', packet);
 });
+
+router.post("/save_blog", async function (req, res) {
+    try {
+        var d = req.body;
+        var filename = "";
+
+        // Image upload
+        if (req.files && req.files.blog_image) {
+            filename = Date.now() + "_" + req.files.blog_image.name;
+            await req.files.blog_image.mv("public/upload/blog/" + filename);
+        }
+
+        var sql = "INSERT INTO blog (blog_image,blog_date,blog_title,blog_para,blog_message) VALUES (?,?,?,?,?)";
+        var result = await exe(sql, [filename, d.blog_date, d.blog_title, d.blog_para, d.blog_message]);
+        // res.send(d);
+        res.redirect("/admin/blog")
+    } catch (err) {
+        console.error("Error saving blog:", err);
+        res.status(500).send("Server Error");
+    }
+});
+
+router.get("/edit_blog/:id", async function (req, res) {
+    var id = req.params.id;
+    var sql = `SELECT * FROM blog WHERE blog_id = ?`;
+    var data = await exe(sql, [id]);
+    var packet = { data };
+    res.render('admin/edit_blog.ejs', packet);
+});
+
+router.post("/update_blog/:id", async function (req, res) {
+    try {
+        const d = req.body;
+        const id = req.params.id;
+
+        // keep old image by default
+        let filename = d.old_blog_image;
+
+        // if new image uploaded
+        if (req.files && req.files.blog_image) {
+            filename = Date.now() + "_" + req.files.blog_image.name;
+            await req.files.blog_image.mv("public/upload/blog/" + filename);
+        }
+
+        const sql = `
+            UPDATE blog
+            SET blog_image = ?,
+                blog_date = ?,
+                blog_title = ?,
+                blog_para = ?,
+                blog_message = ?
+            WHERE blog_id = ?
+        `;
+
+        await exe(sql, [
+            filename,
+            d.blog_date,
+            d.blog_title,
+            d.blog_para,
+            d.blog_message,
+            id
+        ]);
+
+        res.redirect("/admin/blog");
+    } catch (err) {
+        console.error("Error updating blog:", err);
+        res.status(500).send("Server Error");
+    }
+});
+
+router.get("/delete_blog/:id", async function (req, res) {
+    var id = req.params.id;
+    var sql = "DELETE FROM blog WHERE blog_id = ?";
+    var result = await exe(sql, [id]);
+    res.redirect("/admin/blog");
+});
+
+
+router.get("/blog_slider", async function (req, res) {
+    var sql = "SELECT * FROM blog_slider";
+    var data = await exe(sql);
+    var packet = { data };
+    res.render('admin/blog/blog_slider.ejs', packet);
+});
+
+router.post("/blog_slider_save", async function (req, res) {
+    try {
+        var d = req.body;
+        var filename = "";
+
+        // Image upload
+        if (req.files && req.files.blog_image) {
+            filename = Date.now() + "_" + req.files.blog_image.name;
+            await req.files.blog_image.mv("public/upload/blog/" + filename);
+        }
+
+        var sql = `
+            INSERT INTO blog_slider (blog_image, blog_title, blog_description)
+            VALUES (?,?,?)
+        `;
+
+        await exe(sql, [
+            filename,
+            d.blog_title,
+            d.blog_description   // <-- FIXED
+        ]);
+
+        res.redirect("/admin/blog_slider");
+
+    } catch (err) {
+        console.error("Error saving blog slider:", err);
+        res.status(500).send("Server Error");
+    }
+});
+
+router.get("/blog_slider_edit/:id", async function (req, res) {
+    var id = req.params.id;
+    var sql = `SELECT * FROM blog_slider WHERE id = ?`;
+    var data = await exe(sql, [id]);
+    var packet = { data };
+    res.render('admin/blog_slider_edit.ejs', packet);
+});
+
+router.post("/update_blog_slider/:id", async function (req, res) {
+    try {
+        var d = req.body;
+        var id = req.params.id;
+        var filename = d.old_blog_image;
+        // Image upload
+        if (req.files && req.files.blog_image) {
+            filename = Date.now() + "_" + req.files.blog_image.name;
+            await req.files.blog_image.mv("public/upload/blog/" + filename);
+        }
+        var sql = `
+            UPDATE blog_slider
+            SET blog_image = ?, blog_title = ?, blog_description = ?    
+            WHERE id = ?
+        `;
+        await exe(sql, [
+            filename,
+            d.blog_title,
+            d.blog_description,
+            d.id
+        ]);
+        res.redirect("/admin/blog_slider");
+    } catch (err) {
+        console.error("Error updating blog slider:", err);
+        res.status(500).send("Server Error");
+    }
+});
+
+router.get("/blog_slider_status/:id", async function (req, res) {
+    var id = req.params.id;
+    var sql = "DELETE FROM blog_slider  WHERE id=?";
+    var result = await exe(sql, [id]);
+    res.redirect("/admin/blog_slider");
+})
+
 router.get("/booking", function (req, res) {
     res.render('admin/booking.ejs');
 });
--- routes/user_route.js
+++ routes/user_route.js
@@ -7,88 +7,97 @@ router.get("/", function (req, res) {
     res.render('user/home.ejs', { data });
 });
 
-router.get("/about", function (req, res){
+router.get("/about", function (req, res) {
     res.render('user/about.ejs');
 });
 
-router.get("/services", function (req, res){
+router.get("/services", function (req, res) {
     res.render('user/services.ejs');
 });
 
-router.get("/packages", function (req, res){
+router.get("/packages", function (req, res) {
     res.render('user/packages.ejs');
 });
 
-router.get("/gallery", function (req, res){
+router.get("/gallery", function (req, res) {
     res.render('user/gallery.ejs');
 });
 
-router.get("/testmonials", function (req, res){
+router.get("/testmonials", function (req, res) {
     res.render('user/testmonials.ejs');
 });
 
-router.get("/blog", function (req, res){
-    res.render('user/blog.ejs');
+router.get("/blog", async function (req, res) {
+
+    var sql = "SELECT * FROM blog_slider";
+    var blog_slider = await exe(sql);
+
+    var sql2 = "SELECT * FROM blog";
+    var blog = await exe(sql2);
+
+    var packet = { blog_slider, blog };
+
+    res.render('user/blog.ejs', packet);
 });
 
-router.get("/contact", function (req, res){
+router.get("/contact", function (req, res) {
     res.render('user/contact.ejs');
 });
 
-router.get("/privacy", function (req, res){
+router.get("/privacy", function (req, res) {
     res.render('user/privacy.ejs');
 });
 
-router.get("/terms", function (req, res){
+router.get("/terms", function (req, res) {
     res.render('user/terms.ejs');
 });
 
-router.get("/FAQ", function (req, res){
+router.get("/FAQ", function (req, res) {
     res.render('user/FAQ.ejs');
 });
 
-router.get("/login", function (req, res){
+router.get("/login", function (req, res) {
     res.render('user/login.ejs');
 });
 
-router.get("/user_register", function (req, res){
+router.get("/user_register", function (req, res) {
     res.render("user/register.ejs");
 });
 
-router.get("/book_event", function (req, res){
+router.get("/book_event", function (req, res) {
     res.render("user/book_event.ejs");
 });
 
 router.post("/save_user", async function (req, res) {
-  try {
-    var d = req.body;
-    var sql = `INSERT INTO users (name, email, mobile, username, password) VALUES (?, ?, ?, ?, ?)`;
-    await exe(sql, [ d.name, d.email, d.mobile, d.username, d.password]);
-    res.redirect("/login?register=success");
-  } catch (err) {
-    console.log(err);
-    res.send("Server Error");
-  }
+    try {
+        var d = req.body;
+        var sql = `INSERT INTO users (name, email, mobile, username, password) VALUES (?, ?, ?, ?, ?)`;
+        await exe(sql, [d.name, d.email, d.mobile, d.username, d.password]);
+        res.redirect("/login?register=success");
+    } catch (err) {
+        console.log(err);
+        res.send("Server Error");
+    }
 });
 
 
 router.post("/save_login", async function (req, res) {
-  try {
-    var d = req.body;
-    var sql = `SELECT * FROM users WHERE username = ?`;
-    var result = await exe(sql, [d.username]);
-    if (result.length === 0) {
-      return res.redirect("/login?error=invalid");
+    try {
+        var d = req.body;
+        var sql = `SELECT * FROM users WHERE username = ?`;
+        var result = await exe(sql, [d.username]);
+        if (result.length === 0) {
+            return res.redirect("/login?error=invalid");
+        }
+        if (d.password !== result[0].password) {
+            return res.redirect("/login?error=invalid");
+        }
+        req.session.user = result[0];
+        return res.redirect("/?login=success");
+    } catch (err) {
+        console.log(err);
+        res.send("Server Error");
     }
-    if (d.password !== result[0].password) {
-      return res.redirect("/login?error=invalid");
-    }
-    req.session.user = result[0];
-    return res.redirect("/?login=success");
-  } catch (err) {
-    console.log(err);
-    res.send("Server Error");
-  }
 });
 
 
