c49599dc7379ba4e6e07d5a5c88cb32058295559
Gaurav Honde <gauravhonde66@gmail.com>
2026-02-04 11:10:32 +0530

    Footer dynamic

--- connection.js
+++ connection.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 var util = require("util");
 
 var conn = mysql.createConnection({
-    host:"bohcx1sasr4br0vv072c-mysql.services.clever-cloud.com",
-    user:"uzkznabow4ktfdpv",
-    password:"YTMRJf7X5ndJ8Zb6wQwR",
-    database:"bohcx1sasr4br0vv072c"
+    host: "bohcx1sasr4br0vv072c-mysql.services.clever-cloud.com",
+    user: "uzkznabow4ktfdpv",
+    password: "YTMRJf7X5ndJ8Zb6wQwR",
+    database: "bohcx1sasr4br0vv072c"
 });
 
 var exe = util.promisify(conn.query).bind(conn);
